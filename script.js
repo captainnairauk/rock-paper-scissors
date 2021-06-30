b8b2023346850f11e5fac069fe57c8b59e2161c9
Aniket Kaimal <aniket1995@yahoo.co.in>
2021-06-30 09:10:21 +0530

    Replaced if else conditional statements with switch conditional statements.

--- script.js
+++ script.js
@@ -15,30 +15,46 @@ let totalComputerScore=0;
 
 //playRound() function takes two parameters 'playerSelection' & 'computerSelection'- and then returns a string that declares the winner of the round.
 function playRound(playerSelection, computerSelection){
-    if ((playerSelection ==="rock" && computerSelection ==="paper")){
-        playerScore=losePoint, computerScore=winPoint;    
-        return ("You lose this round!! Paper wraps rock." );
-    }else if ((playerSelection ==="")){
+    switch (true){
+        case ((playerSelection ==="rock" && computerSelection ==="paper")):
         playerScore=losePoint, computerScore=winPoint;
-        return("You lose this round!! Next time please type rock or paper or scissors.");
-    }else if ((playerSelection ==="rock" && computerSelection ==="scissors")){
-        playerScore=winPoint, computerScore=losePoint;
-        return("You've won this round!! Rock crushes scissors.");
-    }else if ((playerSelection ==="paper" && computerSelection ==="rock")){
-        playerScore=winPoint, computerScore=losePoint;
-        return("You've won this round!! Paper wraps rock.");
-    }else if ((playerSelection ==="paper" && computerSelection ==="scissors")){
+        console.log ("You lose this round!! Paper wraps rock." );
+        break;
+        
+        case ((playerSelection ==="")):
         playerScore=losePoint, computerScore=winPoint;
-        return ("You've won the round!! Scissors cuts paper.");
-    }else if ((playerSelection ==="scissors" && computerSelection ==="paper")){
+        console.log("You lose this round!! Next time please type rock or paper or scissors.");
+        break;
+
+        case ((playerSelection ==="rock" && computerSelection ==="scissors")):
         playerScore=winPoint, computerScore=losePoint;
-        return ("You've won the round!! Scissors cuts paper.");
-    }else if ((playerSelection==="scissors" && computerSelection==="rock")){
+        console.log("You've won this round!! Rock crushes scissors.");
+        break;
+
+        case((playerSelection ==="paper" && computerSelection ==="rock")):
+        playerScore=winPoint, computerScore=losePoint;
+        console.log("You've won this round!! Paper wraps rock.");
+        break;
+
+        case((playerSelection ==="paper" && computerSelection ==="scissors")):
         playerScore=losePoint, computerScore=winPoint;
-        return ("You lose this round!! Rock crushes scissors.")
-    }else {
+        console.log("You've won the round!! Scissors cuts paper.");
+        break;
+
+        case((playerSelection ==="scissors" && computerSelection ==="paper")):
+        playerScore=winPoint, computerScore=losePoint;
+        console.log("You've won the round!! Scissors cuts paper.");
+        break;
+
+        case((playerSelection==="scissors" && computerSelection==="rock")):
+        playerScore=losePoint, computerScore=winPoint;
+        console.log ("You lose this round!! Rock crushes scissors.");
+        break;
+
+        default:
         playerScore=tiePoint, computerScore=tiePoint;
-        return ("It's a Tie!");
+        console.log ("It's a Tie!");
+        break;
     }   
 }
 //scoreCounter() keeps score & adds previous round's score (cumulative)
@@ -60,7 +76,7 @@ function game (){
         console.log("Computer: "+ computerSelection + "!");
 
     let roundResult=playRound(playerSelection, computerSelection);
-        console.log(roundResult);
+        roundResult;
         scoreCounter();
     }  
 //Rules Popup.
