b8347a02052629cac7cef06f619499be7341eb1d
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2024-12-11 19:30:15 +0530

    Create score variables

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-function getComputerChoice(){
-    let options = ["rock", "paper", "scissors"];
-    let index = Math.floor(Math.random()*3);
-    return options[index];
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  let options = ["rock", "paper", "scissors"];
+  let index = Math.floor(Math.random() * 3);
+  return options[index];
 }
 
-function getHumanChoice(){
-    let userInput;
+function getHumanChoice() {
+  let userInput;
 
-    while(true){
-        userInput = prompt("Enter rock, paper, or scissors").toLowerCase();
-        if((userInput === "rock") || (userInput === "paper") || (userInput === "scissors")){
-            break;
-        }else{
-            alert("Invalid input! Please enter rock, paper or scissors.");
-        }
+  while (true) {
+    userInput = prompt("Enter rock, paper, or scissors").toLowerCase();
+    if (
+      userInput === "rock" ||
+      userInput === "paper" ||
+      userInput === "scissors"
+    ) {
+      break;
+    } else {
+      alert("Invalid input! Please enter rock, paper or scissors.");
     }
+  }
 
-    return userInput;
+  return userInput;
 }
 
 console.log(getHumanChoice());
 console.log(getComputerChoice());
-
