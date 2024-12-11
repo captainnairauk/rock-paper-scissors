72aaa8928f60646e6fdf6a2f7c5a9da7bbe485e8
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2024-12-11 18:38:50 +0530

    Add getComputerChoice() function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello, world!");
+function getComputerChoice(){
+    let options = ["rock", "paper", "scissors"];
+    let index = Math.floor(Math.random()*3);
+    return options[index];
+}
+
+console.log(getComputerChoice());
