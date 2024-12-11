2c0f302c4cca6f3df70f8cc3a8e4ce4058e58f5e
Aniket Kaimal <74839851+captainnairauk@users.noreply.github.com>
2024-12-11 19:22:11 +0530

    Add getHumanChoice() function

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ function getComputerChoice(){
     return options[index];
 }
 
+function getHumanChoice(){
+    let userInput;
+
+    while(true){
+        userInput = prompt("Enter rock, paper, or scissors").toLowerCase();
+        if((userInput === "rock") || (userInput === "paper") || (userInput === "scissors")){
+            break;
+        }else{
+            alert("Invalid input! Please enter rock, paper or scissors.");
+        }
+    }
+
+    return userInput;
+}
+
+console.log(getHumanChoice());
 console.log(getComputerChoice());
+
